f20a6867129583f0ddbeca8b6c8f6a0213933795
evgenff1 <139433911+evgenff1@users.noreply.github.com>
2025-03-17 19:40:49 +0300

    Initial Commit

---
+++ StyleShop/StyleShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleShopApp.swift
+//  StyleShop
+//
+//  Created by Evgeniy Fakhretdinov on 17.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct StyleShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
